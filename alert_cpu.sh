1f6ab9c5235405cb75f8b30ce52910523af818cb
mjk-22 <143551767+mjk-22@users.noreply.github.com>
2024-12-10 18:03:04 -0500

    Update alert_cpu.sh

--- alert_cpu.sh
+++ alert_cpu.sh
@@ -10,3 +10,25 @@ groups:
         annotations:
           summary: "High CPU Usage on {{ $labels.instance }}"
           description: "CPU usage on instance {{ $labels.instance }} has been over 80% for the last 10 minutes."
+scrape_configs:
+  # The job name is added as a label `job=<job_name>` to any timeseries scraped from this config.
+  - job_name: 'prometheus'
+
+    # Override the global default and scrape targets from this job every 5 seconds.
+    scrape_interval: 5s
+    scrape_timeout: 5s
+
+    # metrics_path defaults to '/metrics'
+    # scheme defaults to 'http'.
+
+    static_configs:
+      - targets: ['192.168.2.36:9090']
+
+  - job_name: node
+    # If prometheus-node-exporter is installed, grab stats about the local
+    # machine by default.
+    static_configs:
+      - targets: ['localhost:9100']
+ - job_name: 'kali-linux'
+    static_configs:
+      - targets: ['192.168.2.36:9100']
