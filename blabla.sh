bb2a6c873133ad8801234bbf67e13c8fad76a257
mjk-22 <143551767+mjk-22@users.noreply.github.com>
2024-12-10 23:37:37 -0500

    Update blabla.sh

--- blabla.sh
+++ blabla.sh
@@ -1,22 +1,5 @@
-groups:
-  - name: memory_alerts
-    rules:
-      - alert: HighMemoryUsage
-        expr: (node_memory_MemTotal_bytes - node_memory_MemAvailable_bytes) / node_memory_MemTotal_bytes * 100 > 90
-        for: 5m
-        labels:
-          severity: critical
-        annotations:
-         summary: "High Memory Usage on {{ $labels.instance }}"
-         description: "Memory usage on instance {{ $labels.instance }} is above 90% for the last 5 minutes."
+rule_files:
+  # - "first_rules.yml"
+    - "/etc/prometheus/alerts.yml"
+  # - "second_rules.yml"
 
-  - name: cpu_alerts
-    rules:
-      - alert: HighCPUUsage
-        expr: 100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[1m]))) > 80
-        for: 10m
-        labels:
-          severity: warning
-        annotations:
-          summary: "High CPU Usage on {{ $labels.instance }}"
-          description: "CPU usage on instance {{ $labels.instance }} has been over 80% for the last 10 minutes."
